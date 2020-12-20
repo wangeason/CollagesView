42c847081ad5c696e446a41f0cb528f3627d2b65
yansong <wangeason84@gmail.com>
2020-12-20 17:34:14 -0500

    build and publish

---
+++ CollagesView/src/main/java/io/github/wangeason/CollagesView.kt
@@ -0,0 +1,15 @@
+package io.github.wangeason
+
+import android.content.Context
+import android.view.View
+
+/**
+ * TEST
+ *
+ * @constructor
+ * TEST
+ *
+ * @param context
+ */
+class CollagesView(context: Context?) : View(context) {
+}
